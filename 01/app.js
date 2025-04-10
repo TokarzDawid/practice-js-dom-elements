5dcd6e74f8f56920f0047e7d0cb90e47b87d2eb4
Dawid Tokarz <dawid.tokarz97@gmail.com>
2025-04-10 19:18:08 +0200

    task one structure js structure added

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+const item = document.querySelector(
+  '.comments__item.comments__item--newest'
+);
+
+if (item) {
+  const dataElements = item.querySelectorAll(
+    '[data-info]'
+  );
+  console.log(
+    'Znaleziono elementów z data-info:',
+    dataElements.length
+  );
+}

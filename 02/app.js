3fe6487f011a7a38e3e523531dfa5e330d5aca2a
Dawid Tokarz <dawid.tokarz97@gmail.com>
2025-04-10 19:38:39 +0200

    task 2 structure added

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,23 @@
 console.log('DOM');
+
+const links = document.querySelectorAll(
+  'a[data-url]'
+);
+
+links.forEach(function (link) {
+  const dataUrl = link.getAttribute('data-url');
+
+  if (dataUrl) {
+    link.setAttribute('href', dataUrl);
+  } else {
+    link.addEventListener(
+      'click',
+      function (event) {
+        event.preventDefault();
+        alert(
+          'Ten link nie ma przypisanego adresu'
+        );
+      }
+    );
+  }
+});
